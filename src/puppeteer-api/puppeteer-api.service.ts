9dc16e5a782b11c9e8da04569054a0b78ea67f7f
zhenja <ye.ulchak@insidedynamic.de>
2024-10-07 15:34:04 +0300

    fix

--- src/puppeteer-api/puppeteer-api.service.ts
+++ src/puppeteer-api/puppeteer-api.service.ts
@@ -1,26 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import puppeteer from 'puppeteer';
-// import { CreatePuppeteerApiDto } from './dto/puppeteer-api.dto';
 @Injectable()
 export class PuppeteerApiService {
   
   async GetUser(): Promise<any>{
-    // const browser = await puppeteer.launch();
-    // const page = await browser.newPage();
-  
-    // // Navigate the page to a URL
-    // await page.goto('https://jsonplaceholder.typicode.com');
-  
-    // // Set screen size
-    // await page.setViewport({width: 1080, height: 1024});
-  
-    // // Type into search box
-    // await page.type('.devsite-search-field', 'automate beyond recorder');
+    try {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const posts = response.data;
 
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    console.log('API response:', response.data);
+      // Якщо потрібно, можете перетворити масив в об'єкт, наприклад, за id
+      const postsById = posts.reduce((acc, post) => {
+        acc[post.id] = post; // Встановлюємо id як ключ
+        return acc;
+      }, {});
 
-    const post = response.data
+      console.log('API response:', postsById); // Виводимо об'єкт в консоль
+      return postsById; // Повертаємо об'єкт
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+      throw error; // Можна кинути помилку, щоб обробити її на рівні контролера
+    }
   }
 }
